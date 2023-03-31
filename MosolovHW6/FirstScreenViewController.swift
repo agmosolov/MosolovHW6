153cb4cd0572f02ce46a8b42481135d8c5accc61
Aleksander Mosolov <agmosolov@icloud.com>
2023-03-31 21:51:59 +0300

    UI elements + outlets

---
+++ MosolovHW6/FirstScreenViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  MosolovHW6
+//
+//  Created by Александр Мосолов on 31.03.2023.
+//
+
+import UIKit
+
+class FirstScreenViewController: UIViewController {
+
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    @IBOutlet var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+    @IBAction func loginButtonTapped() {
+    }
+    
+    @IBAction func forgotUserButtonTapped() {
+    }
+   
+    @IBAction func forgotPasswordButtonTapped() {
+    }
+    
+    
+    
+    
+}
+
