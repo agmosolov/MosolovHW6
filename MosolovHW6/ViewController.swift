b5d69d41146b01354b28a912e4ca3af8275e22b5
Aleksander Mosolov <agmosolov@icloud.com>
2023-03-31 21:30:01 +0300

    Initial Commit

---
+++ MosolovHW6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MosolovHW6
+//
+//  Created by Александр Мосолов on 31.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
