16fd356e9c6bcb05b62d2e56e9a12243a011ac3f
Aleksander Mosolov <agmosolov@icloud.com>
2023-03-31 22:19:13 +0300

    Alerts

--- MosolovHW6/FirstScreenViewController.swift
+++ MosolovHW6/FirstScreenViewController.swift
@@ -13,22 +13,58 @@ class FirstScreenViewController: UIViewController {
     @IBOutlet var passwordTF: UITextField!
     @IBOutlet var loginButton: UIButton!
     
+    let userAndPassword = ["User1" : 123]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        
+        loginButton.layer.cornerRadius = 10
+        
     }
 
     @IBAction func loginButtonTapped() {
     }
     
     @IBAction func forgotUserButtonTapped() {
+    
+        let userAlert = UIAlertController(
+            title: "Don't worry!",
+            message: "Your user name is User1",
+            preferredStyle: .alert
+        )
+        
+        let userAlertAction = UIAlertAction(
+            title: "OK",
+            style: .default,
+            handler: nil
+        )
+        
+        userAlert.addAction(userAlertAction)
+        
+        self.present(userAlert, animated: true, completion: nil)
     }
    
+ 
     @IBAction func forgotPasswordButtonTapped() {
+        
+        let userAlert = UIAlertController(
+            title: "Don't worry!",
+            message: "Your password is 123",
+            preferredStyle: .alert
+        )
+        
+        let userAlertAction = UIAlertAction(
+            title: "OK",
+            style: .default,
+            handler: nil
+        )
+        
+        userAlert.addAction(userAlertAction)
+        
+        self.present(userAlert, animated: true, completion: nil)
     }
-    
-    
-    
-    
+        
 }
+    
 
--- MosolovHW6/SecondScreenViewController.swift
+++ MosolovHW6/SecondScreenViewController.swift
@@ -9,21 +9,16 @@ import UIKit
 
 class SecondScreenViewController: UIPageViewController {
 
+    @IBOutlet var welcomeMessageLabel: UILabel!
+    @IBOutlet var logoutButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func logoutButtonTapped() {
     }
-    */
+    
 
 }
